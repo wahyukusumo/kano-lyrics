b06b297f3f1db2c3389de9d6536cc308b7edf27d
Wahyu Kusumo <wahyukusumo32@gmail.com>
2022-08-13 14:21:50 +0700

    change image to access public folder

--- src/components/AlbumHeader.vue
+++ src/components/AlbumHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-nowrap px-1 py-5 items-end">
-        <img v-bind:src="'src/assets/album_cover/' + album.cover" class="object-cover w-24 h-24 lg:w-56 lg:h-56 rounded" alt="" />
+        <img v-bind:src="'/album_cover/' + album.cover" class="object-cover w-24 h-24 lg:w-56 lg:h-56 rounded" alt="" />
         <div class="p-3 md:p-5">
           <h1 class="text-2xl lg:text-5xl font-bold">{{ album.title.romaji }}</h1>
           <h3 class="md:pt-2">{{ album.released }}</h3>
